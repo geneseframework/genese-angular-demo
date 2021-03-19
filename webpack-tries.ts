f1e8fd7d3d96ab3685f0e9841efa4a4c7039d565
Gilles Fabre <gillesfabre34@gmail.com>
2021-03-19 16:27:03 +0100

    Require wp runs

---
+++ webpack-tries.ts
@@ -0,0 +1,11 @@
+
+console.log('AAAaAAAAAAAAAAA');
+const wpp = require('wp');
+// import { numToWord } from 'wp';
+
+
+const num = 5;
+
+const zzz = wpp.numToWord(num);
+console.log('ZZZZ', zzz);
+
